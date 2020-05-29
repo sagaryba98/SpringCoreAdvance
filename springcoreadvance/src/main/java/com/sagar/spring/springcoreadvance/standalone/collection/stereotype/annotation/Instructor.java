b89bfcd1dcb85a418122d49c41fc3e1cb39c99a8
sagarwaiba <sagarwaiba@192.168.0.9>
2020-05-29 15:33:31 -0500

    Added Stereotype annotation

--- springcoreadvance/src/main/java/com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/Instructor.java
+++ springcoreadvance/src/main/java/com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/Instructor.java
@@ -1,5 +1,8 @@
 package com.sagar.spring.springcoreadvance.standalone.collection.stereotype.annotation;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -11,23 +14,46 @@ public class Instructor {
 	private int id;
 	@Value("Sagar")
 	private String name;
+	@Value("#{topics}")
+	private List<String> topics;
+	@Autowired
+	private Profile profile;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public List<String> getTopics() {
+		return topics;
+	}
+
+	public void setTopics(List<String> topics) {
+		this.topics = topics;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
 	@Override
 	public String toString() {
-		return "Instructor [id=" + id + ", name=" + name + "]";
+		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + ", profile=" + profile + "]";
 	}
-	
-	
 
 }
--- springcoreadvance/src/main/java/com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/Test.java
+++ springcoreadvance/src/main/java/com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/Test.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/instructorconfig.xml"
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/sagar/spring/springcoreadvance/standalone/collection/stereotype/annotation/insdustryconfig.xml"
 );
 		Instructor instructor = (Instructor) context.getBean("instructor");
 		System.out.println(instructor);
